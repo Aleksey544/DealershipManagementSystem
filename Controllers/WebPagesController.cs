a84f09392e399300b21785145bd3ccbe10e5eb86
Aleksey544 <hackercorporation544@gmail.com>
2023-03-07 19:17:00 +0200

    electic car page was added

---
+++ Controllers/WebPagesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DealershipManagementSystem
+{
+    public class WebPagesController : Controller
+    {
+		public ViewResult ElectricCarCharacteristics()
+		{
+			return View();
+		}
+
+		public ViewResult ElectricCarsCatalog()
+        {
+            return View();
+        }
+    }
+}
